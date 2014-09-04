6dc3784a2889e536a586f5abdbafc8d33decf141
Tracy Jones <tjones@vmware.com>
2014-09-04 14:40:24 -0700

    Added details to Readme and updated run-report with comments

--- run-report.sh
+++ run-report.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# This script is used by a cron job to run the report on a regular basis and refresh the data in a place
+# where the apache server to pick up
+
 cd ~ubuntu/bugs
 python nova-bugs.py
 cp bugs-refresh.json /var/www/bugs.json
