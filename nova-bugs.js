b192c29d45cda554dab4fb612aac025dbd9001d4
Tracy Jones <tjones@vmware.com>
2014-09-11 09:19:14 -0700

    Added radio button for bugs with owners which have not been updated in 30 days

--- nova-bugs.js
+++ nova-bugs.js
@@ -2,39 +2,38 @@
 
 var app = angular.module("NovaBugs", []);
 
-app.controller("BugCtrl", function($scope) {
+app.controller("BugCtrl", function ($scope) {
     $scope.priority = "Critical";
     $scope.orderList = "importance";
     $scope.filterSelection = "all";
-    $scope.data = angular.fromJson(FileHelper.readStringFromFileAtPath ( "bugs.json" ));
+    $scope.data = angular.fromJson(FileHelper.readStringFromFileAtPath("bugs.json"));
     $scope.raw_data = $scope.data.bugs;
     $scope.date = $scope.data.date;
     $scope.reverseSort = false;
 
     console.log($scope.date);
-    for( var i=0, l=$scope.raw_data.length; i<l; i++ ) {
+    for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
         var owner = $scope.raw_data[i].owner;
         if (owner != "Unknown")
             $scope.raw_data[i].parsed_owner = owner.substring(31);
         else
             $scope.raw_data[i].parsed_owner = owner;
         try {
-	    $scope.raw_data[i].status_prio = "status" + $scope.raw_data[i].status.toUpperCase().replace(" ", "");
-	}
-	catch (exception) {
-	    $scope.raw_data[i].status_prio = "unknown";
-	}
-	try {
-	    $scope.raw_data[i].prio_class = "importance" + $scope.raw_data[i].importance.toUpperCase();
-	}
-	catch (exception) {
-	    $scope.raw_data[i].importance_prio = "unknown";
-	}
+            $scope.raw_data[i].status_prio = "status" + $scope.raw_data[i].status.toUpperCase().replace(" ", "");
+        }
+        catch (exception) {
+            $scope.raw_data[i].status_prio = "unknown";
+        }
+        try {
+            $scope.raw_data[i].prio_class = "importance" + $scope.raw_data[i].importance.toUpperCase();
+        }
+        catch (exception) {
+            $scope.raw_data[i].importance_prio = "unknown";
+        }
         $scope.raw_data[i].merged = 0;
         $scope.raw_data[i].abandoned = 0;
         $scope.raw_data[i].in_review = 0;
-        for (var j= 0, jl=$scope.raw_data[i].reviews.length; j<jl; j++)
-        {
+        for (var j = 0, jl = $scope.raw_data[i].reviews.length; j < jl; j++) {
             if ($scope.raw_data[i].reviews[j].status == "MERGED")
                 $scope.raw_data[i].merged++;
             else if ($scope.raw_data[i].reviews[j].status == "ABANDONED")
@@ -46,26 +45,29 @@ app.controller("BugCtrl", function($scope) {
     $scope.filtered_data = $scope.raw_data;
 
 
-    $scope.FilterCtrl = function(value) {
-	//$scope.filterSelection = value;
-	$scope.filtered_data = [];
-	console.log($scope.filterSelection);
-	if ($scope.filterSelection == "all"){
-	    $scope.filtered_data = $scope.raw_data;
-	}
-	else {
-	    if ($scope.filterSelection == "stale") {
-		filterStale();
-	    }
-	    else if ($scope.filterSelection == "update") {
-		filterUpdated();
-	    }
-	    else if ($scope.filterSelection == "inProgress") {
-		filterInProgress();
-	    }
-	    else if ($scope.filterSelection == "noOwner") {
-		filterNoOwner();
-	    }
+    $scope.FilterCtrl = function (value) {
+        //$scope.filterSelection = value;
+        $scope.filtered_data = [];
+        console.log($scope.filterSelection);
+        if ($scope.filterSelection == "all") {
+            $scope.filtered_data = $scope.raw_data;
+        }
+        else {
+            if ($scope.filterSelection == "stale") {
+                filterStale();
+            }
+            else if ($scope.filterSelection == "update") {
+                filterUpdated();
+            }
+            else if ($scope.filterSelection == "inProgress") {
+                filterInProgress();
+            }
+            else if ($scope.filterSelection == "noOwner") {
+                filterNoOwner();
+            }
+            else if ($scope.filterSelection == "ownerAbandoned") {
+                filterOwnerAbandoned();
+            }
             else if ($scope.filterSelection == "abandoned") {
                 filterAbandoned();
             }
@@ -79,11 +81,11 @@ app.controller("BugCtrl", function($scope) {
                 filterUndecided();
             }
 
-	}
+        }
     };
 
     function filterUndecided() {
-        for( var i=0, l=$scope.raw_data.length; i<l; i++ ) {
+        for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
             var item = $scope.raw_data[i];
             if (item.importance == "Undecided") {
                 $scope.filtered_data.push(item);
@@ -95,7 +97,7 @@ app.controller("BugCtrl", function($scope) {
         filterInProgress();
         data = $scope.filtered_data;
         $scope.filtered_data = [];
-        for( var i=0, l=data.length; i<l; i++ ) {
+        for (var i = 0, l = data.length; i < l; i++) {
             if (data[i].in_review > 0)
                 $scope.filtered_data.push(data[i]);
         }
@@ -105,7 +107,7 @@ app.controller("BugCtrl", function($scope) {
         filterInProgress();
         data = $scope.filtered_data;
         $scope.filtered_data = [];
-        for( var i=0, l=data.length; i<l; i++ ) {
+        for (var i = 0, l = data.length; i < l; i++) {
             if (data[i].merged == 0 && data[i].abandoned > 0 && data[i].in_review == 0)
                 $scope.filtered_data.push(data[i]);
         }
@@ -115,7 +117,7 @@ app.controller("BugCtrl", function($scope) {
         filterInProgress();
         data = $scope.filtered_data;
         $scope.filtered_data = [];
-        for( var i=0, l=data.length; i<l; i++ ) {
+        for (var i = 0, l = data.length; i < l; i++) {
             if (data[i].merged > 0 && data[i].abandoned == 0 && data[i].in_review == 0)
                 $scope.filtered_data.push(data[i]);
         }
@@ -123,48 +125,55 @@ app.controller("BugCtrl", function($scope) {
 
 
     function filterStale() {
-	for( var i=0, l=$scope.raw_data.length; i<l; i++ ) {
-	    var item = $scope.raw_data[i];
-	    if (item.stale == 1) {
-		$scope.filtered_data.push(item);
-	    }
-	}
+        for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
+            var item = $scope.raw_data[i];
+            if (item.stale == 1) {
+                $scope.filtered_data.push(item);
+            }
+        }
     }
 
     function filterUpdated() {
-	for( var i=0, l=$scope.raw_data.length; i<l; i++ ) {
-	    var item = $scope.raw_data[i];
-	    if (item.never_touched == 1) {
-		$scope.filtered_data.push(item);
-	    }
-	}
+        for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
+            var item = $scope.raw_data[i];
+            if (item.never_touched == 1) {
+                $scope.filtered_data.push(item);
+            }
+        }
     }
 
     function filterInProgress() {
-	for( var i=0, l=$scope.raw_data.length; i<l; i++ ) {
-	    var item = $scope.raw_data[i];
-	    if (item.status == "In Progress") {
-		$scope.filtered_data.push(item);
-	    }
-	}
+        for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
+            var item = $scope.raw_data[i];
+            if (item.status == "In Progress") {
+                $scope.filtered_data.push(item);
+            }
+        }
     }
 
     function filterNoOwner() {
-	for( var i=0, l=$scope.raw_data.length; i<l; i++ ) {
-	    var item = $scope.raw_data[i];
-	    if (item.owner == "None") {
-		$scope.filtered_data.push(item);
-	    }
-	}
+        for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
+            var item = $scope.raw_data[i];
+            if (item.owner == "None") {
+                $scope.filtered_data.push(item);
+            }
+        }
+    }
+    function filterOwnerAbandoned() {
+        for (var i = 0, l = $scope.raw_data.length; i < l; i++) {
+            var item = $scope.raw_data[i];
+            if (item.owner != "None" && item.stale == 1) {
+                $scope.filtered_data.push(item);
+            }
+        }
     }
 
 });
 
-function FileHelper()
-{}
+function FileHelper() {
+}
 {
-    FileHelper.readStringFromFileAtPath = function(pathOfFileToReadFrom)
-    {
+    FileHelper.readStringFromFileAtPath = function (pathOfFileToReadFrom) {
         var request = new XMLHttpRequest();
         request.open("GET", pathOfFileToReadFrom, false);
         request.send(null);
